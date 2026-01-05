a06a99654c6ea03f42a024bf5aa3c549b6f381ee
Yuichi Sugiyama <ozwald176@gmail.com>
2026-01-05 21:57:39 +0900

    feat: remove Suspense wrapper from TopPage for simplified rendering

--- src/app/(list)/page.tsx
+++ src/app/(list)/page.tsx
@@ -1,12 +1,7 @@
 import TopPageSection from '@/app/(list)/_components/TopPageSection'
-import { Suspense } from 'react'
 
 export const dynamic = 'force-dynamic'
 
 export default function TopPage() {
-  return (
-    <Suspense fallback={<p>読み込み中...</p>}>
-      <TopPageSection />
-    </Suspense>
-  )
+  return <TopPageSection />
 }
