d240634cc8d8d5e1d85f50fdcf0bc4604877c764
Yuichi Sugiyama <ozwald176@gmail.com>
2026-01-14 21:53:21 +0900

    feat: replace logo images with SVG component for improved scalability

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,20 +1,11 @@
-'use client'
-
 import { ModeToggle } from '@/components/mode-toggle'
-import Image from 'next/image'
-import { useTheme } from 'next-themes'
+import { Logo } from '@/components/icon/Logo'
 
 export const Header = () => {
-  const { theme } = useTheme()
-
   return (
     <header className="w-full h-16 bg-background border-b border-border flex items-center p-4 md:px-6">
       <div className="grow pt-2">
-        {theme === 'dark' ? (
-          <Image src="/logo-dark.svg" alt="artlyst tokyo のロゴ" width="170" height="23" />
-        ) : (
-          <Image src="/logo-light.svg" alt="artlyst tokyo のロゴ" width="170" height="23" />
-        )}
+        <Logo />
       </div>
       <ModeToggle />
     </header>
