b9401d94eee4038a3cee3ca66fc8ea7d8294c02a
Yuichi Sugiyama <ozwald176@gmail.com>
2026-01-14 16:38:41 +0900

    feat: add dark and light theme logos and update header to use theme-based logo

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,9 +1,21 @@
+'use client'
+
 import { ModeToggle } from '@/components/mode-toggle'
+import Image from 'next/image'
+import { useTheme } from 'next-themes'
 
 export const Header = () => {
+  const { theme } = useTheme()
+
   return (
     <header className="w-full h-16 bg-background border-b border-border flex items-center p-4 md:px-6">
-      <h1 className="text-xl font-semibold grow">Artlyst Tokyo</h1>
+      <div className="grow pt-2">
+        {theme === 'dark' ? (
+          <Image src="/logo-dark.svg" alt="artlyst tokyo のロゴ" width="170" height="23" />
+        ) : (
+          <Image src="/logo-light.svg" alt="artlyst tokyo のロゴ" width="170" height="23" />
+        )}
+      </div>
       <ModeToggle />
     </header>
   )
